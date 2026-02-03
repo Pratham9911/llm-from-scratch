5135db59aec51d7b61df61e4910311a3a758815f
Pratham9911 <prathamtiwari0123@gmail.com>
2026-02-03 09:19:17 +0530

    updated sample

--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
-print("Hello")
+import random
+
+a = random.randint(1, 10)
+
+b = int(input("Enter a number between 1 and 10: "))
+if a == b:
+    print("You guessed it!")
+else:
+    print(f"Wrong! The correct number was {a}.")
