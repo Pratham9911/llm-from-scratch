28132b810c6c687929f656d738fb3e53fdbee001
Pratham9911 <prathamtiwari0123@gmail.com>
2026-02-03 09:37:38 +0530

    Updated Coded

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 import random
-import ctypes
+import time
 import os
 
 a = random.randint(1, 10)
@@ -8,6 +8,8 @@ b = int(input("Enter a number between 1 and 10: "))
 if a == b:
     print("You guessed it!")
 else:
-    print("Wrong! Turning off screen...")
-    os.system("xset dpms force off")
+    print("Wrong! Terminal locked 😈")
 
+    while True:
+        print("ACCESS DENIED")
+        time.sleep(2)
